76cab9d002603e118123bf09e6415fd816e07a93
Sns Charmie <71021496+charlsoluwapelumi@users.noreply.github.com>
2022-06-03 12:44:49 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,12 +10,14 @@
 
 int main(void)
 {
-int i = 97;
-while (i < 123)
-{
-putchar(i);
-i++;
-}
-putchar(10);  
-return (0);
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
 }
