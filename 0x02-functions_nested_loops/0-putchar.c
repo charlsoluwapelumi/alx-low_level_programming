1ed472bb35b329b038b62595895969a78ee1fd9c
Sns Charmie <71021496+charlsoluwapelumi@users.noreply.github.com>
2022-06-08 11:57:17 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - prints Holberton
+ * main - prints putchar
  *
- * Description: prints Holberton
+ * Description: prints putchar
  *
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-	char text[10] = "_putchar";
+	char text[9] = "_putchar";
 	int i = 0;
 
 	for (i = 0; i < 9; i++)
