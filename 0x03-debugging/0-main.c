13c7527601065700f84e8b9a9712c8ba2dd1c608
Sns Charmie <71021496+charlsoluwapelumi@users.noreply.github.com>
2022-06-09 22:58:24 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,14 +9,8 @@ int main(void)
 {
         int i;
 
-        i = 98;
+        i = 0;
         positive_or_negative(i);
-        if (i == 0)
-                printf("%d is zero\n", n);
-        else if (i < 0)
-                printf("%d is negative\n", n);
-        else
-                printf("%d is positive\n", n);
 
         return (0);
 }
