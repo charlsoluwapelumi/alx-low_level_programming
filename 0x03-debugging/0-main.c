45eb9fd3a125c58422ceb1de3a9eb48cb87ef311
Sns Charmie <71021496+charlsoluwapelumi@users.noreply.github.com>
2022-06-09 22:51:32 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,20 @@
-#include <time.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void positive_or_negative(int i){
-int n;
-srand(time(0));
-n = i;
-if (n == 0)
-printf("%d is zero\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
-else
-printf("%d is positive\n", n);
+void positive_or_negative(int i)
+{
+        int n;
+        srand(time(0));
+        n = i;
+        if (n == 0)
+                printf("%d is zero\n", n);
+        else if (n < 0)
+                printf("%d is negative\n", n);
+        else
+                printf("%d is positive\n", n);
 
-return (0);
+        return (0);
 }
 
 /**
