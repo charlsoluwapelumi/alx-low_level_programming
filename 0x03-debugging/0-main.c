edd578c88b381cd30daeb19ba6a018fe9acecbeb
Sns Charmie <71021496+charlsoluwapelumi@users.noreply.github.com>
2022-06-09 21:37:33 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,15 @@
 #include "main.h"
+#include <stdlib.h>
+#include <time.h>
+
 
 void positive_or_negative(int i)
 {
+        int n;
+
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
         if (n == 0)
         printf("%d is zero\n", n);
         else if (n < 0)
