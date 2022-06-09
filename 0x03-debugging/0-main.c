9565e9efc18dcc92f02ea77637fc7b8f38f25a30
Sns Charmie <71021496+charlsoluwapelumi@users.noreply.github.com>
2022-06-09 21:27:21 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+void positive_or_negative(int i)
+{
+        if (n == 0)
+        printf("%d is zero\n", n);
+        else if (n < 0)
+        printf("%d is negative\n", n);
+        else
+        printf("%d is positive\n", n);
+        return (0);
+}
+
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -9,7 +20,7 @@ int main(void)
 {
         int i;
 
-        i = 98;
+        i = 0;
         positive_or_negative(i);
 
         return (0);
