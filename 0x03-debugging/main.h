03f10bf292da1475d82e3ab2668f6f05f72aecdb
Sns Charmie <71021496+charlsoluwapelumi@users.noreply.github.com>
2022-06-10 13:35:37 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,14 @@
 
 #include <stdio.h>
 
+/**
+ * File - main.h
+ * Desc: Header file containing declaration functiions used in
+ * 	 0x03-debugging directory.
+ */
+
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
